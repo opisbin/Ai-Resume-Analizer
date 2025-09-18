4df59dd66bafc3f05fa5bc32eaf078cb66d6170e
opisbin <hmeherab243@gmail.com>
2025-09-18 16:06:41 +0600

    Add authentication handling and loading state to Auth and Home components

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,3 +1,7 @@
+import { usePuterStore } from "lib/puter";
+import { use, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+
 export const meta = () => {
     return [
         { title: "Resumind | Auth" },
@@ -6,13 +10,45 @@ export const meta = () => {
 }
 
 const Auth = () => {
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next]);
+
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
-                    <div className="flex flex-col item-center gap-2 text-center">
+                    <div className="flex flex-col items-center gap-2 text-center">
                         <h1>Welcome!</h1>
                         <h2>Log in to Continue Your Job Journey</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse" disabled>
+                                <p>Signing you in ...</p>
+                            </button>
+                        ) : (
+                            <>
+                               {auth.isAuthenticated ?(
+
+                                <button className="auth-button" onClick={auth.signOut}>
+                                <p>Log Out</p>
+                                </button>
+
+                                 ) : (
+
+                                <button className="auth-button" onClick={auth.signIn}>
+                                <p>Log in</p>
+                                </button>
+
+                               )}
+                            </>
+                        )}
                     </div>
                 </section>
             </div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import type { Resume } from "../../types";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "lib/puter";
+import { use, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +15,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated, next]);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
 
     <Navbar />
